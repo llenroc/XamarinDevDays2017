c61d003ab366418bd8e139a99163135ab44c7b7c
UsuarioLocal <UsuarioLocal>
2017-07-29 15:44:30 -0600

    Agregar archivos de proyecto.

---
+++ XamarinDevDay/XamarinDevDay/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinDevDay
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            boton1.Clicked += (s, e) => Continuar();
+		}
+
+        private async void Continuar()
+        {
+            if (switch1.IsToggled)
+            {
+                await Navigation.PushAsync(new MainPage());
+                return;
+            }
+            await DisplayAlert("hola " + texto1.Text, "App movil", "Cerrar");
+        }
+	}
+}
